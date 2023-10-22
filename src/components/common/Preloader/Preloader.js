3a58a159863b9a2c1905224f1aad36372bb8a3ac
Serhii <875dfsb6435278785@gmail.com>
2023-10-22 17:04:49 +0200

    mapDispatchToProps

---
+++ src/components/common/Preloader/Preloader.js
@@ -0,0 +1,10 @@
+import React from "react";
+import preloader from "../../../assets/images/load-8510_128.gif";
+
+let Preloader = (props) => {
+    return <div>
+        <img src={preloader}/>
+    </div>
+}
+
+export default Preloader;
